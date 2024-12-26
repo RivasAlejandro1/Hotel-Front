03e0207887152f5b8c42db4eec3caa05206b4664
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-12-26 13:10:22 -0600

    add styles and font-FontFamily

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import styles from "App.module.css";
-import './App.css'
+import styles from "./App.module.css";
+import Nav from './components/Nav/Nav';/* 
+import Home from './views/Home/Home'; */
 
 function App() {
 
   return (
     <div className={styles.App}>
-    
+    <Nav></Nav>{/* 
+    <Home></Home> */}
       {/* <Routes>
          <Route path="/" element={<Login/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
