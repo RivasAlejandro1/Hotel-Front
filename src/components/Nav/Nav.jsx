6a5df2aec83d8ec442e017eed7bc0e4f040d4603
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-12-26 13:13:38 -0600

    NavBar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import NavButton from "../NavButton/NavButton";
+import style from './Nav.module.css';
+
+
+export default function Nav (){
+    const currentUser = useSelector(state => state.user);
+
+
+    return(
+        <nav className={style.Nav}>
+            <div  className={style.logo} >CMB</div>
+
+            <ul>
+                {  
+                    currentUser ?              
+                    <NavButton path="myTurns" name="myTurns"></NavButton>
+                    : <></>
+                }  
+                <NavButton path="home" name="Home"></NavButton>
+                <NavButton path="about" name="About"></NavButton>
+                <NavButton path="register" name="Register"></NavButton>
+                <NavButton path="/" name="Log in"></NavButton>
+            </ul>
+
+            <button className={style.button}>
+                Contact Us
+            </button>
+        </nav>
+    )
+}
