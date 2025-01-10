87d3da35061ce01a284aa618ab9b0150ae8c3d89
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-10 12:24:48 -0600

    Nav Component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
 import NavButton from "../NavButton/NavButton";
 import style from './Nav.module.css';
-
+import logo from '../../assets/logo1.jpg';
+import Login from "../Login/Login";
 
 export default function Nav (){
     const currentUser = useSelector(state => state.user);
@@ -9,7 +10,9 @@ export default function Nav (){
 
     return(
         <nav className={style.Nav}>
-            <div  className={style.logo} >CMB</div>
+            <div  className={style.logo} >
+                <img src={logo}></img>
+            </div>
 
             <ul>
                 {  
@@ -17,15 +20,19 @@ export default function Nav (){
                     <NavButton path="myTurns" name="myTurns"></NavButton>
                     : <></>
                 }  
-                <NavButton path="home" name="Home"></NavButton>
-                <NavButton path="about" name="About"></NavButton>
-                <NavButton path="register" name="Register"></NavButton>
+                <NavButton path="Inicio" name="Inicio"></NavButton>
+                <NavButton path="Acerca de" name="Acerca de"></NavButton>
+{/*                 <NavButton path="Contacto" name="Contacto"></NavButton>
+ */}            <NavButton path="Habitaciones" name="Habitaciones"></NavButton>
+                <NavButton path="Registrarse" name="Registrarse"></NavButton>
+                <NavButton path="Servicios" name="Servicios"></NavButton>
                 <NavButton path="/" name="Log in"></NavButton>
             </ul>
 
             <button className={style.button}>
-                Contact Us
+                Contactanos 
             </button>
+          {/*   <Login></Login> */}
         </nav>
     )
 }
