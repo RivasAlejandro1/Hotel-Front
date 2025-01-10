7f3348ba9f38acde04807b0bc552652f3a28c24e
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-10 12:27:36 -0600

    HeaderInfo Component

--- src/components/HeaderInfo/HeaderInfo.jsx
+++ src/components/HeaderInfo/HeaderInfo.jsx
@@ -1,21 +1,8 @@
 import style from './HeaderInfo.module.css'
-import myImage from '../../helpers/line 19.png'; 
 import image2 from '../../assets/2.jpg'; 
-import image3 from '../../assets/3.jpg'; 
-import image4 from '../../assets/4.jpg'; 
 import image5 from '../../assets/5.jpg'; 
-import image6 from '../../assets/6.jpg'; 
-import image7 from '../../assets/7.jpg'; 
 import image8 from '../../assets/8.jpg'; 
-import image9 from '../../assets/9.jpg'; 
-import image10 from '../../assets/10.jpg'; 
-import image11 from '../../assets/11.jpg'; 
-import image12 from '../../assets/12.jpg'; 
-import image13 from '../../assets/13.jpg'; 
-import image14 from '../../assets/14.jpg'; 
 import image15 from '../../assets/15.jpg'; 
-import image16 from '../../assets/16.jpg'; 
-import image17 from '../../assets/17.jpg'; 
 export default function HeaderInfo (e) {
 
     return(
@@ -33,16 +20,16 @@ export default function HeaderInfo (e) {
                     <img src={image5}></img>
                 </div>
                <div className={`${style.text} ${style.text_1}`}>
-                    <span>24</span>
-                    <p>Years of experience</p>
+                    <span>07</span>
+                    <p>Años de Atención</p>
                 </div>
                <div className={`${style.text} ${style.text_2}`}>
-                    <span>162</span>
-                    <p>Projects completed</p>
+                    <span>07</span>
+                    <p>Cantidad de Habitaciones</p>
                 </div>
                 <div className={`${style.littleForm} ${style.littleForm_2}`}>
 
-                    <p>Make your dream home.</p>
+                    <p>¿Preguntas? CONTACTANOS</p>
                     <input
                          placeholder='Your email address'
                     ></input>
