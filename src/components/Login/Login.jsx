3910e1ee1b97c4693e121a8fe798692bc868cc45
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-27 14:47:18 -0600

    Modife Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,95 @@
 import style from './Login.module.css';
-import img1 from '../../assets/1.jpg';
-export default function Login() {
-    return(
-        <form className={style.Login}>
-                <label for="email"></label>
-                <input
-                    id="email"
-                    name="Email o Username"
-                    placeholder='Email o Username'
-                ></input>
-                <label for="password"></label>
-                <input
-                        placeholder='Password'
-                ></input>
+import { useForm }  from 'react-hook-form';
+import { emailRegex,emailRegexMessage } from '../../helpers/validateEmail';
+import { validatePassword } from '../../helpers/validatePassword';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
-                <button className={style.button}>
-                    Iniciar sesión 
-                </button>
+const API_ENDPOINT = "http://localhost:3000/login"
+
+export default function Login(e) {
+
+    const { register, handleSubmit, formState: {errors}} = useForm();
+    const Navigate = useNavigate()
+
+    const onSubmit = handleSubmit((data) => {
+        const {password, email} = data;
+        const info = {password, email};
+        
+        try {
+            axios.post(API_ENDPOINT, info).then(
+                ({data}) => {
+                    window.localStorage.setItem(
+                        'loggedIdAppUser', JSON.stringify(data.id)
+                    );
+                    window.localStorage.setItem(
+                        'loggedTokenAppUser', JSON.stringify(data.token)
+                    );
+                    window.localStorage.setItem(
+                        'loggedTypeAppUser', JSON.stringify(data.administrador)
+                    );
+                    Navigate("/")
+           })
+
+        }catch(error){
+            console.log(error.response.data)
+        }
+       
+    })
+
+    return(
+        <form 
+            onSubmit={onSubmit} 
+            className={
+                e.Apper ? 
+                style.Login
+                : style.LogInDisApper
+                }
+        >
+                <label
+                    htmlFor='email' 
+                ></label>
+                <input
+                    type='text'
+                    id="email"
+                    name="email"
+                    placeholder='Email'
+                    {...register("email", {
+                        required: {
+                            value: true,
+                            message: "Correo es requerido"
+                        },
+                        pattern: {
+                            value: emailRegex,
+                            message: emailRegexMessage
+                        },
+                    })}
+                ></input>
+                {
+                    errors.email ?  <span className={style.errors}>{errors.email.message}</span> : <></>
+                }
+                <label
+                    htmlFor='password'
+                ></label>
+                <input
+                    type='password'
+                    id="password"
+                    name="password"
+                    placeholder='Password'
+                    
+                    {...register("password", {
+                        required: {
+                            value: true,
+                            message: "Password es requerido"
+                        },
+                        validate: (value)=> validatePassword(value)
+                    })}
+                ></input>
+                {
+                    errors.password ?  <span className={style.errors}>{"Complete la contraseña"}</span> : <></>
+                }
+
+                <button className={style.button} > Iniciar sesión </button>
                 <div className={style.link}>
                     <a className={style.button}>
                                 Crear una nueva cuenta 
@@ -25,4 +98,4 @@ export default function Login() {
 
         </form>
     )
-}
+}   
