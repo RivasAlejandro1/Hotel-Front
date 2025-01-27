dd0931a281ca2f62f45801574a5c4478cfd5c439
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-27 14:51:45 -0600

    modifed Reservations

--- src/views/Reservations/Reservations.jsx
+++ src/views/Reservations/Reservations.jsx
@@ -1,36 +1,65 @@
+import { useEffect, useState } from 'react';
 import ReservationCard from '../../components/ReservationCard/ReservationCard';
 import style from './Reservations.module.css';
+import getAllUserReservertionAxios from './getAllUserReservertionAxios';
 
-export default function Reservations (e) {
+export default function Reservations () {
+    const [allReservations, setAllReservations] = useState([]);
+
+    const TextType = (type) =>{
+        const TypeMessage = {
+            Sencillo: "Cama Matrimonial y cama individual, baño privado, balcón.",
+            Doble: "Cama Matrimonial y cama individual, baño privado, balcón.",
+            Familiar: "Tres camas matrimoniales y una litera, baño privado, balcón con vista al mar."
+        }
+
+        if(type =="Sencillo")return TypeMessage.Sencillo
+        if(type =="Doble")return TypeMessage.Doble
+        if(type =="Familiar")return TypeMessage.Familiar
+        return ""
+    }
+
+
+    useEffect(()=>{
+        const loadedReservation = async () =>{
+            const loggedIdUserJSON = localStorage.getItem('loggedIdAppUser')
+            const id = JSON.parse(loggedIdUserJSON);
+            const AllFindedReservations = await getAllUserReservertionAxios(id);
+            setAllReservations(AllFindedReservations);
     
+        }
+        loadedReservation();
+    },[])
 
     return(
+
         <section className={style.Reservations}>
             <div className={style.shape}>
                 <h1>Reservaciones</h1>
             </div>
             <div className={style.InfoContainer}>
                 <div className={style.shape1}></div>
-                <ReservationCard
-                    title="Sencilla"
-                    text="Cama Matrimonial y cama individual, baño privado, balcón."
-                    number="1"
-                    room="1"
-                
-                    DateEntry="2025-01-10"
-                    DateOut="2025-01-11"
-                    price="80"
-                ></ReservationCard>
-                <ReservationCard
-                    title="Familiar"
-                    text="Tres camas matrimoniales y una litera, baño privado, balcón con
-                vista al mar."
-                    number="3"
-                    price="200"
-                    room="4"
-                    DateEntry="2025-01-11"
-                    DateOut="2025-01-12"
-                ></ReservationCard>
+                {
+                    allReservations?.map((reservation)=>(
+                        <ReservationCard
+                            key={reservation.id}
+                            title={reservation.room.tipo}
+                            text= {TextType}
+                            number={reservation.id}
+                            room={reservation.room.numero}
+                        
+                            DateEntry={reservation.entryDate}
+                            DateOut={reservation.entryDate}
+                            price={reservation.room.precio}
+                        ></ReservationCard>
+
+                    ))
+                }
+                {
+                    allReservations.length === 0 ?  
+                    <div className={style.Nothing}><h2>No tiene Reservaciones</h2></div> 
+                    : <></>
+                }
             </div>
         </section>
         
