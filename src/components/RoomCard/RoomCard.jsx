b9328ef0196d79f93ac15649e5ff5a99582ad953
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-27 14:48:51 -0600

    Modified Cards

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,6 +1,4 @@
 import style from './RoomCard.module.css';
-import img1 from '../../assets/1.jpg'
-
 import img2 from '../../assets/vectors/turista.png';
 import img3 from '../../assets/vectors/dolar.png';
 
@@ -9,39 +7,66 @@ import Sencilla from '../../assets/7.jpg';
 import Doble from '../../assets/16.jpg';
 import Familiar from '../../assets/12.jpg';
 
+
+
+import ReservationForm from '../ReservationForm/ReservationForm';
+import { useState } from 'react';
+
+
+
 export default function RoomCard(e) {
+
+    
+    
+    
+    //* AppearForm
+    const [activeReserve, setActiveReserve] = useState(false);
+    const appearReserveForm  = (state = true) => {
+        setActiveReserve(state);
+    }
+
+    
+   
+
     return(
-        <div className={style.RoomCard}>
-            <div className={style.ImgContainer}>
-                <img src={
-                    e.title == "Sencilla" ?
-                    Sencilla : 
-                    e.title == "Doble" ?
-                    Doble : 
-                    Familiar 
-                }></img>
-                <div className={style.shadow}></div>
-            </div>
+        <>
+            {/* //! ReserveForm */} 
 
+           { activeReserve ? <ReservationForm close={appearReserveForm} numbers={e.numbers}></ReservationForm>: <></>}
 
-
-
-            <div className={style.InfoContainer}>
-                <h2>{e.title}</h2>
-                <p>{e.text}</p>
-                <div className={style.spaceInfoContainer}>
-                    <img src={img2} ></img>
-                    <p>{e.number} huéspedes</p>
+            {/* //! RoomCard */} 
+            <div className={style.RoomCard}>
+                <div className={style.ImgContainer}>
+                    <img src={
+                        e.title == "Sencilla" ?
+                        Sencilla : 
+                        e.title == "Doble" ?
+                        Doble : 
+                        Familiar 
+                    }></img>
+                    <div className={style.shadow}></div>
                 </div>
-                <div className={style.spaceInfoContainer}>
-                    <img src={img3} ></img>
-                    <p>{e.price}</p>
-                    
+
+
+
+
+                <div className={style.InfoContainer}>
+                    <h2>{e.title}</h2>
+                    <p>{e.text}</p>
+                    <div className={style.spaceInfoContainer}>
+                        <img src={img2} ></img>
+                        <p>{e.number} huéspedes</p>
+                    </div>
+                    <div className={style.spaceInfoContainer}>
+                        <img src={img3} ></img>
+                        <p>{e.price}</p>
+                        
+                    </div>
+                    <button onClick={()=>appearReserveForm()} >
+                        Reservar
+                    </button>
                 </div>
-                <button >
-                    Reservar
-                </button>
             </div>
-        </div>
+        </>
     )
 }
