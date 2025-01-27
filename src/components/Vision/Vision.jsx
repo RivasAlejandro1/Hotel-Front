9919d3a4b265f9321e696ec6cb72ab307385bb1f
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-27 14:49:59 -0600

    modifed Vision

--- src/components/Vision/Vision.jsx
+++ src/components/Vision/Vision.jsx
@@ -3,29 +3,27 @@ import img1 from '../../assets/5.jpg';
 import img2 from '../../assets/15.jpg';
 export default function Vision() {
     return(
-        <section className={style.vision}>
-            <div className={style.degradado}></div>
+        <article className={style.vision}>
             <h1>Visión</h1>
             <div className={style.form1}>
-                <img src= {img1}></img>
+                <img src= {img1} alt="Foto Alberca"></img>
             </div>
             <div className={style.form2}>
-                <img src={img2}></img>
+                <img src={img2} alt="Foto Sala de estar"></img>
             </div>
-            <div className={style.form3}>
-                <p>
-                Convertirse en la posada turística familiar de playa colorada por excelencia y ser
+            <div className={style.form3} title='shape3'>
+                <p>Convertirse en la posada turística familiar de playa colorada por excelencia y ser
                 ampliamente reconocidos en la zona, no solo por sus instalaciones, sino también
                 por contar con la más alta calidad de servicio, además de una razonable relación
-                calidad-precio y un inigualable servicio personalizado para sus huéspedes.                </p>
-                <div className={style.buttonContainer}>
+                calidad-precio y un inigualable servicio personalizado para sus huéspedes.</p>
+                {/* <div className={style.buttonContainer}>
                     <button  className={style.button}>
                         About Us
                     </button>
 
-                </div>
-{/*                 <img src={arrow} className={style.imgArrow}></img>
- */}        </div>
-        </section>
+                </div> */}
+                {/*                 <img src={arrow} className={style.imgArrow}></img> */}  
+      </div>
+        </article>
     )
 }
